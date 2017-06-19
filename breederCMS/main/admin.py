892139ce84bb5be709daab1f1483e0ede1cb83df
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-06-19 22:06:16 +0200

    model implemented

--- breederCMS/main/admin.py
+++ breederCMS/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import AnimalEntry
 
 # Register your models here.
+
+admin.site.register(AnimalEntry)
--- breederCMS/main/models.py
+++ breederCMS/main/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class AnimalEntry(models.Model):
+
+    AVAILABLE = 'A'
+    RESERVED = 'R'
+    SOLD = 'S'
+    STATUS = (
+        (AVAILABLE, 'Available'),
+        (RESERVED, 'Reserved'),
+        (SOLD, 'Sold'),
+    )
+
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDER = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female')
+    )
+
+    TABBY = 'TA'
+    COLORPOINT = 'CP'
+    COLOR = ((TABBY, 'Tabby'), (COLORPOINT, 'Colorpoint'))
+
+    name = models.CharField(max_length=50)
+    date_of_birth = models.DateField('date of birth')
+    gender = models.CharField(max_length=1, choices=GENDER, default=MALE)
+    status = models.CharField(max_length=1, choices=STATUS, default=AVAILABLE)
+    color = models.CharField(max_length=2, choices=COLOR, default=TABBY)
+    picture = models.ImageField(blank=True)
+    description = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name + self.gender + self.status + self.color + self.description
